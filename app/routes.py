5dc5a19b6bb007cb5fb9af90a7cbe89e60abb9da
Maxim Godkin <mgodkin@gmail.com>
2018-03-01 23:06:19 +0300

    create db users and posts

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,6 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
-import request
 import requests, bs4, smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
