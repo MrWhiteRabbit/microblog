f978e7ef164f079895fd6968ee12c7ae1b0b3b80
Maxim Godkin <mgodkin@gmail.com>
2018-03-04 20:47:51 +0300

    added active user, make auth model

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
 import requests, bs4, smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Max'}
+    #user = {'username': 'Max'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -25,22 +29,36 @@ def index():
             'body': 'Какая гадость эта ваша заливная рыба!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+          return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+       user = User.query.filter_by(username=form.username.data).first()
+       if user is None or not user.check_password(form.password.data):
+           flash('Неправильные Имя или Пароль!')
+           return redirect(url_for('login'))
+       login_user(user, remember=form.remember_me.data)
+       next_page = request.args.get('next')
+       if not next_page or url_parse(next_page).netloc != '':
+           next_page = url_for('index')
+       return redirect(url_for('index'))
     return render_template('login.html', title='Авторизация', form=form)
 
-
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/w', methods=['POST'])
+@login_required
 def w():
     fromaddr = "mg.test.robot@gmail.com"
     mypass = "27RoBot27"
@@ -111,12 +129,12 @@ def w():
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
     server.quit()
-    user = {'username': 'Max'}
-    form = LoginForm()
-    return render_template('weather.html', title='Рассылка погоды', user=user, form=form)
+
+    return redirect(url_for('index'))
 
 
 @app.route('/weather', methods=['GET', 'POST'])
+@login_required
 def weather():
     user = {'username': 'Max'}
     form = LoginForm()
