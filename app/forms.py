7a320269069c9a6bd1a15ecac555d133ea84fc5e
Maxim Godkin <mgodkin@gmail.com>
2018-03-05 12:23:49 +0300

    added register func, refactoring code of weather func

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('UserName', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remeber Me')
-    submit = SubmitField('Sign In')
+    username = StringField('Имя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить')
+    submit = SubmitField('Войти')
+
+class WeatherForm(FlaskForm):
+    submit = SubmitField('Отправить погоду')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста используйте другое имя пользователя.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста используйте другой email.')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm
-import requests, bs4, smtplib
+from app import app, db
+from app.forms import LoginForm, WeatherForm, RegistrationForm
+import requests, smtplib
+from bs4 import BeautifulSoup as bs
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from flask_login import current_user, login_user, logout_user, login_required
@@ -14,7 +15,6 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required
 def index():
-    #user = {'username': 'Max'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -37,18 +37,18 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-          return redirect(url_for('index'))
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-       user = User.query.filter_by(username=form.username.data).first()
-       if user is None or not user.check_password(form.password.data):
-           flash('Неправильные Имя или Пароль!')
-           return redirect(url_for('login'))
-       login_user(user, remember=form.remember_me.data)
-       next_page = request.args.get('next')
-       if not next_page or url_parse(next_page).netloc != '':
-           next_page = url_for('index')
-       return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Неправильные Имя или Пароль!')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Авторизация', form=form)
 
 @app.route('/logout')
@@ -57,12 +57,31 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Поздравляем, вы зарегистрированы!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 @app.route('/w', methods=['POST'])
 @login_required
 def w():
-    fromaddr = "mg.test.robot@gmail.com"
-    mypass = "27RoBot27"
+#    form = WeatherForm()
+#    user = User.query.filter_by(username=form.username.data).first()
+    fromaddr = 'mg.test.robot@gmail.com'
+    mypass = '27RoBot27'
     toaddr = "mgodkin@gmail.com"
+#    toaddr = user.email
 
     city_arr = [
         'москва',
@@ -76,52 +95,48 @@ def w():
 
     for el in range(0, len(city_arr)):
         city = city_arr[el]
-        s = requests.get('https://sinoptik.com.ru/погода-' + city)
-        b = bs4.BeautifulSoup(s.text, 'html.parser')
+        rqst = requests.get('https://sinoptik.com.ru/погода-' + city)
+        soup = bs(rqst.text, 'html.parser')
 
-        p3 = b.select('.temperature .p3')
-        pogoda1 = p3[0].getText()
+        p3 = soup.select('.temperature .p3')
+        weather1 = p3[0].getText()
 
-        p4 = b.select('.temperature .p4')
-        pogoda2 = p4[0].getText()
+        p4 = soup.select('.temperature .p4')
+        weather2 = p4[0].getText()
 
-        p5 = b.select('.temperature .p5')
-        pogoda3 = p5[0].getText()
+        p5 = soup.select('.temperature .p5')
+        weather3 = p5[0].getText()
 
-        p6 = b.select('.temperature .p6')
-        pogoda4 = p6[0].getText()
+        p6 = soup.select('.temperature .p6')
+        weather4 = p6[0].getText()
 
-        p = b.select('.rSide .description')
-        pogoda = p[0].getText()
+        p = soup.select('.rSide .description')
+        weather = p[0].getText()
 
         f.write('----------------------------------' + '\n' +
-                pogoda.strip() + '\n' +
-                'Утром: ' + pogoda1 + ' ' + pogoda2 + '\n' +
-                'Вечером: ' + pogoda3 + ' ' + pogoda4 + '\n')
+                weather.strip() + '\n' +
+                'Утром: ' + weather1 + ' ' + weather2 + '\n' +
+                'Вечером: ' + weather3 + ' ' + weather4 + '\n')
 
-    np = b.select('.oDescription .rSide .description')
-    nar_pogoda = np[0].getText()
+    pw = soup.select('.oDescription .rSide .description')
+    people_weather = pw[0].getText()
 
     f.write('----------------------------------' + '\n' +
-            '\n' + nar_pogoda.strip() + '\n' + '\n' +
+            '\n' + people_weather.strip() + '\n' + '\n' +
             '----------------------------------' + '\n' + '\n' +
             'Хорошего всем настроения! Ваш робот.' + '\n')
 
     f.close()
 
-
-    f = open('weather.txt', 'r')
-
-    l = f.read()
-
-    f.close()
+    with open('weather.txt', 'r') as f:
+        msg_body = f.read()
 
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
     msg['Subject'] = 'Привет от погодного робота'
 
-    msg.attach(MIMEText(str(l), 'plain'))
+    msg.attach(MIMEText(str(msg_body), 'plain'))
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
@@ -136,9 +151,8 @@ def w():
 @app.route('/weather', methods=['GET', 'POST'])
 @login_required
 def weather():
-    user = {'username': 'Max'}
-    form = LoginForm()
-    return render_template('weather.html', title='Рассылка погоды', user=user, form=form)
+    form = WeatherForm()
+    return render_template('weather.html', title='Рассылка погоды', form=form)
 
 
 if __name__ == '__main__':
