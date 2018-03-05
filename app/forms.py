be4aad90799ba59f6bbddb6e8186adbdd7a1fb78
Maxim Godkin <mgodkin@gmail.com>
2018-03-05 14:28:30 +0300

    add profile func, editprofile func, gravatar func

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -28,3 +28,9 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Пожалуйста используйте другой email.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Имя', validators=[DataRequired()])
+    about_me = TextAreaField('Обо мне', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Сохранить')
